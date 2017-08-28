18abb96ddb5c57e2723f6d7193fc51a60eeda7db
由依 <yinjian.688@gmail.com>
2017-08-28 16:12:37 +0800

    Create multi_thread.py

---
+++ multi_thread.py
@@ -0,0 +1,38 @@
+import threading,random,queue,random
+queue = []
+
+con = threading.Condition()
+
+class Producer(threading.Thread):
+    def run(self):
+        while True:
+            if con.acquire():
+                if len(queue) > 100:
+                    con.wait()
+                else:
+                    elem = random.randrange(100)
+                    queue.append(elem)
+                    print("Producer a elem {}, Now size is {}".format(elem, len(queue)))
+                    time.sleep(random.random())
+                    con.notify()
+                con.release()
+
+class Consumer(threading.Thread):
+    def run(self):
+        while True:
+            if con.acquire():
+                if len(queue) < 0:
+                    con.wait()
+                else:
+                    elem = queue.pop()
+                    print("Consumer a elem {}. Now size is {}".format(elem, len(queue)))
+                    time.sleep(random.random())
+                    con.notify()
+                con.release()
+
+def main():
+    for i in range(3):
+        Producer().start()
+
+    for i in range(2):
+        Consumer().start()
